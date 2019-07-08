5a8c89a418b7a44235b7a6387995dacd66c08034
Mateus Schmitz <mateus@tiendanube.com>
2019-07-08 15:35:29 -0300

    Removing unused files

--- docs/testes_shell.sh
+++
@@ -1,2 +0,0 @@
-curl --header "Content-Type: text/xml;charset=UTF-8" --header "SOAPAction:getValor" --data @request.xml https://www3.bcb.gov.br/wssgs/services/FachadaWSSGS
-curl -H "Content-Type: text/xml; charset=utf-8" -H "SOAPAction:getValor" -d @request.xml -X POST https://www3.bcb.gov.br/wssgs/services/FachadaWSSGS
